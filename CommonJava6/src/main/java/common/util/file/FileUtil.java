bb186c2fd268e3d7b75f1aa9b799797b8f7a4b2b
김대광 <@DESKTOP-KDK>
2018-03-21 00:39:52 +0900

    정리

--- CommonJava6/src/main/java/common/util/file/FileUtil.java
+++ CommonJava6/src/main/java/common/util/file/FileUtil.java
@@ -353,12 +353,17 @@ public class FileUtil {
 	public static List<String> getFileList(String filePath) {
 		List<String> listFiles = new ArrayList<String>();
 		File file = new File(filePath);
-		File[] files = file.listFiles();
 		
-		for (File f : files) {
-			if (f.isFile()) {
-				listFiles.add(f.getName());
+		if (file.isDirectory()) {
+			File[] files = file.listFiles();
+			
+			for (File f : files) {
+				if (f.isFile()) {
+					listFiles.add(f.getName());
+				}
 			}
+		} else {
+			listFiles.add(file.getName());
 		}
 		
 		return listFiles;
@@ -372,11 +377,14 @@ public class FileUtil {
 	public static List<String> getDirectoryList(String filePath) {
 		List<String> listDirectories = new ArrayList<String>();
 		File file = new File(filePath);
-		File[] files = file.listFiles();
 		
-		for (File f : files) {
-			if (f.isDirectory()) {
-				listDirectories.add(f.getName());
+		if (file.isDirectory()) {
+			File[] files = file.listFiles();
+			
+			for (File f : files) {
+				if (f.isDirectory()) {
+					listDirectories.add(f.getName());
+				}
 			}
 		}
 		
