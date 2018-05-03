efc5eae2e89e04ed40050729199eabff860d8524
김대광 <@DESKTOP-FSRD4MA>
2018-05-03 20:41:25 +0900

    정리

--- CommonJava6/src/main/java/common/spring/resolver/ParamCollector.java
+++ CommonJava6/src/main/java/common/spring/resolver/ParamCollector.java
@@ -111,11 +111,12 @@ public class ParamCollector {
 		return map.getBoolean(key);
 	}
 	
+	@SuppressWarnings("unchecked")
 	public List<Object> getArray(String key) {
-		@SuppressWarnings("unchecked")
-		List<Object> list = (List<Object>) map.get(key);
-		
-		if (list == null) {
+		List<Object> list = null;
+		try {
+			list = (List<Object>) map.get(key);
+		} catch (ClassCastException e) {
 			list = new ArrayList<Object>();
 			list.add(map.get(key));
 		}
