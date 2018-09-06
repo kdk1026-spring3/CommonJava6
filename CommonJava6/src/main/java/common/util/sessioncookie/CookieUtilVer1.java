b125ce6e8ef6935a222b5802485de5ab16609d4c
김대광 <@DESKTOP-FSRD4MA>
2018-09-06 21:57:02 +0900

    정리

--- CommonJava6/src/main/java/common/util/sessioncookie/CookieUtilVer1.java
+++ CommonJava6/src/main/java/common/util/sessioncookie/CookieUtilVer1.java
@@ -1,30 +1,14 @@
 package common.util.sessioncookie;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class CookieUtilVer1 {
-	
+
 	private CookieUtilVer1() {
 		super();
 	}
-	
-	private static final Logger logger = LoggerFactory.getLogger(CookieUtilVer1.class);
-
-	private static final String CHARSET = "UTF-8";
 
 	/**
 	 * Servlet 2.5 쿠키 설정
@@ -34,37 +18,48 @@ public class CookieUtilVer1 {
 	 * @param maxAge
 	 * @param isUseJs
 	 */
-	public static void addCookie(HttpServletResponse response, String cookieName, String cookieValue, int maxAge, boolean isUseJs) {
+	public static void addCookie(HttpServletResponse response, String cookieName, String cookieValue, int maxAge, boolean isUseJs, boolean isSecure) {
+		Cookie cookie = new Cookie(cookieName, cookieValue);
+		cookie.setMaxAge(maxAge);
+		cookie.setPath("/");
+		
+		if (isSecure) {
+			cookie.setSecure(true);
+		}
+
 		if (isUseJs) {
-			Cookie cookie = new Cookie(cookieName, cookieValue);
-			cookie.setMaxAge(maxAge);
-			cookie.setPath("/");
-			
 			response.addCookie(cookie);
-			
+
 		} else {
-			Date expdate = new Date();
-			expdate.setTime (expdate.getTime() + (1000 * maxAge));
-			DateFormat df = new SimpleDateFormat("EEE, dd-MMM-yyyy HH:mm:ss zzz", Locale.ENGLISH);
-			df.setTimeZone(TimeZone.getTimeZone("GMT"));
-			String cookieExpire = df.format(expdate);
-			
-			try {
-				StringBuilder sb = new StringBuilder();
-				sb.append(cookieName);
-				sb.append("=");
-				sb.append(URLEncoder.encode(cookieValue, CHARSET));
-				sb.append("; Expires=");
-				sb.append(cookieExpire);
-				sb.append("; Path=/");
-				sb.append("; HttpOnly");
-				
-				response.addHeader("Set-Cookie", sb.toString());
-				
-			} catch (UnsupportedEncodingException e) {
-				logger.error("", e);
+			StringBuilder sb = new StringBuilder();
+			sb.append(cookie.getName());
+			sb.append("=").append(cookie.getValue());
+			sb.append("; Expires=").append(cookie.getMaxAge());;
+			sb.append("; Path=").append(cookie.getPath());
+			sb.append("; HttpOnly");
+
+			response.setHeader("Set-Cookie", sb.toString());
+		}
+	}
+
+	/**
+	 * cookieName 인자 값을 가지는 쿠키 가져오기
+	 * @param request
+	 * @param cookieName
+	 * @return
+	 */
+	public static Cookie getCookie(HttpServletRequest request, String cookieName) {
+		Cookie resCookie = null;
+		Cookie[] cookies = request.getCookies();
+
+		for (final Cookie c : cookies) {
+			if ( cookieName.equals(c.getName()) && !"".equals(c.getValue()) ) {
+				resCookie = c;
+				break;
 			}
 		}
+
+		return resCookie;
 	}
 
 	/**
@@ -73,25 +68,17 @@ public class CookieUtilVer1 {
 	 * @param cookieName
 	 * @return
 	 */
-	public static String getCookie(HttpServletRequest request, String cookieName) {
+	public static String getCookieValue(HttpServletRequest request, String cookieName) {
 		String cookieValue = "";
-		Cookie[] cookies = request.getCookies();
+		Cookie cookie = getCookie(request, cookieName);
 
-		if (cookies != null && cookies.length > 0) {
-			for (int i=0; i < cookies.length; i++) {
-				if ( cookieName.equals(cookies[i].getName()) ) {
-					try {
-						cookieValue = URLDecoder.decode(cookies[i].getValue(), CHARSET);
-					} catch (UnsupportedEncodingException e) {
-						logger.error("", e);
-					}
-				}
-			}
+		if ( cookie != null ) {
+			cookieValue = cookie.getValue();
 		}
-		
+
 		return cookieValue;
 	}
-	
+
 	/**
 	 * 모든 쿠키 제거
 	 * @param request
@@ -99,12 +86,12 @@ public class CookieUtilVer1 {
 	 */
 	public static void removeCookies(HttpServletRequest request, HttpServletResponse response) {
 		Cookie[] cookies = request.getCookies();
-		
+
 		if (cookies != null && cookies.length > 0) {
 			for (int i=0; i < cookies.length; i++) {
 				cookies[i].setPath("/");
 				cookies[i].setMaxAge(0);
-				
+
 				response.addCookie(cookies[i]);
 			}
 		}
@@ -121,7 +108,26 @@ public class CookieUtilVer1 {
 		cookie.setPath("/");
 		cookie.setMaxAge(0);
 		
+		if ( cookie.getSecure() ) {
+			cookie.setSecure(true);
+		}
+
 		response.addCookie(cookie);
+	}
+
+	public static boolean isExist(HttpServletRequest request, String cookieName) {
+		String cookieValue = getCookieValue(request, cookieName);
+		return !"".equals(cookieValue);
+	}
+
+	public static int getCookieMaxAge(HttpServletRequest request, String cookieName) {
+		Cookie cookie = getCookie(request, cookieName);
+
+		if ( cookie == null ) {
+			return 0;
+		} else {
+			return cookie.getMaxAge();
+		}
 	}
 
 }
